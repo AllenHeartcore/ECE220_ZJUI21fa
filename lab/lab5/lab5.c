1c233d12b0ae35298fe53a6f82a051f0ccdf5e0e
Chen, Ziyuan <ziyuanc3@illinois.edu>
2021-10-19 11:19:29 +0800

    LAB5 completed by Chen, Ziyuan on 19 Oct 2021.

--- lab/lab5/lab5.c
+++ lab/lab5/lab5.c
@@ -1,24 +1,35 @@
 #include <stdint.h>
 #include <stdio.h>
-
 #include "lab5.h"
+#define MASK  0b10000000000000000000000000000000
+#define MATCH 0b11111111111111111111111111111111
+#define MUTEX 0b00000000000000000000000000000000
 
-/*
- * The fit_pieces function determines whether the "pieces" (bit patterns,
- * with some number of leading 0s) can "fit together" (line up with exactly
- * one 1 in any bit position, filling all 32 bits with 1s) when shifted
- * left by appropriate amounts (without overflow / loss of any 1 bits).
- *
- * If such shifts can be found, the function prints the left shift amounts
- * for each of the three pieces in order--p1, p2, and p3--then returns 1.
- * If no such shifts exist, the function returns 0.
- */
-int32_t
-fit_pieces (uint32_t p1, uint32_t p2, uint32_t p3)
-{
-    // write your code here
-
-    // not possible
+int32_t fit_pieces (uint32_t p1, uint32_t p2, uint32_t p3) {
+    uint32_t p2_store = p2, p3_store = p3;
+    uint8_t p1_overflow = 0;
+    for (uint8_t p1_shift = 0; p1_shift <= 31; p1_shift++) {
+        if (p1_overflow == 1) break;
+        p2 = p2_store;
+        uint8_t p2_overflow = 0;
+        for (uint8_t p2_shift = 0; p2_shift <= 31; p2_shift++) {
+            if (p2_overflow == 1) break;
+            p3 = p3_store;
+            uint8_t p3_overflow = 0;
+            for (uint8_t p3_shift = 0; p3_shift <= 31; p3_shift++) {
+                if (p3_overflow == 1) break;
+                if (((p1 | p2 | p3) == MATCH) && ((p1 & p2 & p3) == MUTEX)) {
+                    printf("Shift values are %2d, %2d and %2d.\n", p1_shift, p2_shift, p3_shift);
+                    return 1;
+                }
+                if ((p3 & MASK) == MASK) p3_overflow = 1;
+                p3 <<= 1;
+            }
+            if ((p2 & MASK) == MASK) p2_overflow = 1;
+            p2 <<= 1;
+        }
+        if ((p1 & MASK) == MASK) p1_overflow = 1;
+        p1 <<= 1;
+    }
     return 0;
 }
-
