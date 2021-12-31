e0308549b84c2bf029eb2400211c0fda8a06b5d8
Chen, Ziyuan <ziyuanc3@illinois.edu>
2021-12-31 10:20:30 +0800

    MP11 completed by Chen, Ziyuan on 31 Dec 2021.

--- mp/mp11/mp11.c
+++ mp/mp11/mp11.c
@@ -1,3 +1,22 @@
+/*	###### Copyright Information ######
+	This is the program for ECE220FA21 @ ZJUI Institute, Machine Programming Problem XI. 
+		Written			Commented		Debugged		Commited
+		17, 30 Dec 2021		31 Dec 2021		30, 31 Dec 2021		31 Dec 2021
+	The following 11 functions are written by Steve Lumetta. 
+		gen_long_branch		gen_statement		gen_debug_marker	gen_expression
+		gen_comparison		gen_op_cmp_ne		gen_op_cmp_less		gen_op_cmp_le
+		gen_op_cmp_eq		gen_op_cmp_ge		gen_op_cmp_greater
+	Copyright © 2021 Lumetta, Steve and Chen, Ziyuan. 
+
+	###### Functionality ######
+	Compile by running:	make; make clean
+	Execute by running:	cat tests/${PROGRAM_NAME}.c | ./c220 > ${PROGRAM_NAME}.asm
+	The program outputs:	A .asm code file named "${PROGRAM_NAME}.asm"
+
+	###### Function Documentation ######
+	Refer to respective function headers. 
+ */
+
 /*									tab:8
  *
  * mp11.c - skeleton code generation for ECE190 compiler MP (F08 MP5)
@@ -104,7 +123,7 @@ static void gen_op_cmp_greater (ast220_t* ast);
  * static int foo;  <-- a file scope variable ("static" means only this file)
  *
  */
-
+static ece220_label_t* return_label = NULL;
 
 
 /* 
@@ -121,9 +140,19 @@ static void gen_op_cmp_greater (ast220_t* ast);
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-void 
-MP11_generate_code (ast220_t* prog)
-{
+
+void MP11_generate_code (ast220_t* prog) {
+	if (!return_label) return_label = label_create();		// Init. return_label in the 1st entry
+	switch (prog->type) {						// Distributor
+		case AST220_FOR_STMT:
+		case AST220_IF_STMT:
+		case AST220_RETURN_STMT:
+		case AST220_POP_STACK:
+		case AST220_DEBUG_MARKER: gen_statement(prog); break;	// 5 kinds of statements
+		default: gen_expression(prog); break;			// 25 kinds of expressions
+	}
+	if (prog->next) MP11_generate_code(prog->next);			// Generate code for the next node
+	else printf("%s\n", label_value(return_label));			// Print final return label
 }
 
 /*
@@ -172,11 +201,11 @@ static void
 gen_statement (ast220_t* ast)
 {
     switch (ast->type) {
-        case AST220_FOR_STMT:     gen_for_statement (ast);    break;
-        case AST220_IF_STMT:      gen_if_statement (ast);     break;
-        case AST220_RETURN_STMT:  gen_return_statement (ast); break;
-        case AST220_POP_STACK:    gen_pop_stack (ast);        break;
-        case AST220_DEBUG_MARKER: gen_debug_marker (ast);     break;
+	case AST220_FOR_STMT:     gen_for_statement (ast);    break;
+	case AST220_IF_STMT:      gen_if_statement (ast);     break;
+	case AST220_RETURN_STMT:  gen_return_statement (ast); break;
+	case AST220_POP_STACK:    gen_pop_stack (ast);        break;
+	case AST220_DEBUG_MARKER: gen_debug_marker (ast);     break;
 	default: fputs ("BAD STATEMENT TYPE\n", stderr);      break;
     }
 }
@@ -189,9 +218,22 @@ gen_statement (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_for_statement (ast220_t* ast)
-{
+static void gen_for_statement (ast220_t* ast) {
+	ece220_label_t* loop_label = label_create();
+	ece220_label_t* break_label = label_create();
+	if (ast->left) gen_statement(ast->left);			// Loop init. (may be NULL)
+	printf("%s\n", label_value(loop_label));
+	gen_expression(ast->test);					// Loop test
+	printf("\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R0,#0\n");
+	gen_long_branch(2, break_label);				// Break out of loop
+	ast220_t* current_stmt = ast->middle;
+	while (current_stmt) {
+		gen_statement(current_stmt);				// Loop body
+		current_stmt = current_stmt->next;
+	}
+	if (ast->right) gen_statement(ast->right);			// Loop update
+	gen_long_branch(7, loop_label);					// Break back to loop test
+	printf("%s\n", label_value(break_label));
 }
 
 /* 
@@ -202,9 +244,30 @@ gen_for_statement (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_if_statement (ast220_t* ast)
-{
+static void gen_if_statement (ast220_t* ast) {
+	ece220_label_t* false_label = label_create();
+	ece220_label_t* cont_label;
+	gen_expression(ast->test);					// Test
+	printf("\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R0,#0\n");
+	gen_long_branch(2, false_label);				// Break to false
+	ast220_t* current_stmt = ast->left;
+	while (current_stmt) {
+		gen_statement(current_stmt);				// Tested true
+		current_stmt = current_stmt->next;
+	}
+	if (ast->right) {
+		cont_label = label_create();
+		gen_long_branch(7, cont_label);				// Break to continue
+	}
+	printf("%s\n", label_value(false_label));
+	if (ast->right) {
+		current_stmt = ast->right;
+		while (current_stmt) {
+			gen_statement(current_stmt);			// Tested false
+			current_stmt = current_stmt->next;
+		}
+		printf("%s\n", label_value(cont_label));
+	}
 }
 
 /* 
@@ -215,9 +278,10 @@ gen_if_statement (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_return_statement (ast220_t* ast)
-{
+static void gen_return_statement (ast220_t* ast) {
+	gen_expression(ast->left);					// Calc. return value
+	printf("\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tSTR R0,R5,#3\n");
+	gen_long_branch(7, return_label);				// Break to end of program
 }
 
 /* 
@@ -229,9 +293,9 @@ gen_return_statement (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_pop_stack (ast220_t* ast)
-{
+static void gen_pop_stack (ast220_t* ast) {
+	gen_expression(ast->left);
+	printf("\tADD R6,R6,#1\n");
 }
 
 /* 
@@ -264,10 +328,10 @@ static void
 gen_expression (ast220_t* ast)
 {
     switch (ast->type) {
-        case AST220_PUSH_INT:     gen_push_int (ast);       break;
-        case AST220_PUSH_STR:     gen_push_str (ast);       break;
-        case AST220_VARIABLE:     gen_push_variable (ast);  break;
-        case AST220_FUNC_CALL:    gen_func_call (ast);      break;
+	case AST220_PUSH_INT:     gen_push_int (ast);       break;
+	case AST220_PUSH_STR:     gen_push_str (ast);       break;
+	case AST220_VARIABLE:     gen_push_variable (ast);  break;
+	case AST220_FUNC_CALL:    gen_func_call (ast);      break;
 	case AST220_GET_ADDRESS:  gen_get_address (ast);    break;
 	case AST220_OP_ASSIGN:    gen_op_assign (ast);      break;
 	case AST220_OP_PRE_INCR:  gen_op_pre_incr (ast);    break;
@@ -302,9 +366,11 @@ gen_expression (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_push_int (ast220_t* ast)
-{
+static void gen_push_int (ast220_t* ast) {
+	ece220_label_t* value_label = label_create();
+	ece220_label_t* instr_label = label_create();
+	printf("\tLD R0,%s\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n\tBRnzp %s\n%s\n\t.FILL #%d\n%s\n", 	// R0 - Value
+		label_value(value_label), label_value(instr_label), label_value(value_label), ast->value, label_value(instr_label));
 }
 
 /* 
@@ -316,9 +382,11 @@ gen_push_int (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_push_str (ast220_t* ast)
-{
+static void gen_push_str (ast220_t* ast) {
+	ece220_label_t* value_label = label_create();
+	ece220_label_t* instr_label = label_create();
+	printf("\tLEA R0,%s\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n\tBRnzp %s\n%s\n\t.STRINGZ %s\n%s\n", 	// R0 - Addr. of string
+		label_value(value_label), label_value(instr_label), label_value(value_label), ast->name, label_value(instr_label));
 }
 
 /* 
@@ -330,9 +398,16 @@ gen_push_str (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_push_variable (ast220_t* ast)
-{
+static void gen_push_variable (ast220_t* ast) {
+	symtab_entry_t* variable_entry = symtab_lookup(ast->name);					// Lookup variable
+	if (!ast->left) {
+		printf("\tLDR R0,R%d,#%d\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n", 				// R0 - Value of variable
+			5 - variable_entry->is_global, variable_entry->offset);
+	} else {
+		gen_expression(ast->left);								// R1 - Array offset -> Addr. of variable
+		printf("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R%d,#%d\n\tADD R1,R1,R0\n\tLDR R0,R1,#0\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n", 
+			5 - variable_entry->is_global, variable_entry->offset);
+	}
 }
 
 /* 
@@ -345,9 +420,27 @@ gen_push_variable (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_func_call (ast220_t* ast)
-{
+static void gen_func_call (ast220_t* ast) {
+	ast220_t* args = ast->left;
+	int32_t args_count = 0;
+	while (args) {
+		gen_expression(args);									// Calc. arguments
+		args = args->next;
+		args_count++;
+	}
+	ece220_label_t* subrt_label = label_create();
+	ece220_label_t* instr_label = label_create();
+	printf("\tLD R0,%s\n\tJSRR R0\n\tBRnzp %s\n%s\n\t.FILL ", 					// R0 - Addr. of target function
+		label_value(subrt_label), label_value(instr_label), label_value(subrt_label));
+	switch (ast->fnum) {
+		case AST220_PRINTF:	printf("PRINTF\n");	break;
+		case AST220_RAND:	printf("RAND\n");	break;
+		case AST220_SCANF:	printf("SCANF\n");	break;
+		case AST220_SRAND:	printf("SRAND\n");	break;
+		default: break;
+	}
+	printf("%s\n\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tADD R6,R6,#%d\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n", // R0 - Return value of function
+		label_value(instr_label), args_count);
 }
 
 /* 
@@ -359,9 +452,16 @@ gen_func_call (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_get_address (ast220_t* ast)
-{
+static void gen_get_address (ast220_t* ast) {
+	symtab_entry_t* variable_entry = symtab_lookup(ast->left->name);				// Lookup variable
+	if (!ast->left->left) {
+		printf("\tADD R0,R%d,#%d\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n", 				// R0 - Addr. of variable
+			5 - variable_entry->is_global, variable_entry->offset);
+	} else {
+		gen_expression(ast->left->left);							// R1 - Array offset
+		printf("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R%d,#%d\n\tADD R0,R1,R0\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n", 
+			5 - variable_entry->is_global, variable_entry->offset);
+	}
 }
 
 /* 
@@ -375,9 +475,17 @@ gen_get_address (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_assign (ast220_t* ast)
-{
+static void gen_op_assign (ast220_t* ast) {
+	symtab_entry_t* variable_entry = symtab_lookup(ast->left->name);				// Lookup variable
+	gen_expression(ast->right);									// R1 - Assigned value
+	if (!ast->left->left) {										// Scalar case
+		printf("\tADD R0,R%d,#%d\n\tLDR R1,R6,#0\n\tSTR R1,R0,#0\n", 				// R0 - Addr. of variable
+			5 - variable_entry->is_global, variable_entry->offset);
+	} else {											// Array case
+		gen_expression(ast->left->left);							// R1 - Array offset
+		printf("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R%d,#%d\n\tADD R0,R1,R0\n\tLDR R1,R6,#0\n\tSTR R1,R0,#0\n", 
+			5 - variable_entry->is_global, variable_entry->offset);
+	}
 }
 
 /* 
@@ -390,9 +498,10 @@ gen_op_assign (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_pre_incr (ast220_t* ast)
-{
+static void gen_op_pre_incr (ast220_t* ast) {
+	symtab_entry_t* variable_entry = symtab_lookup(ast->left->name);				// Lookup variable
+	printf("\tADD R0,R%d,#%d\n\tLDR R1,R0,#0\n\tADD R1,R1,#1\n\tSTR R1,R0,#0\n\tADD R6,R6,#-1\n\tSTR R1,R6,#0\n", 
+		5 - variable_entry->is_global, variable_entry->offset);					// R0 - Addr., R1 - Value
 }
 
 /* 
@@ -405,9 +514,10 @@ gen_op_pre_incr (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_pre_decr (ast220_t* ast)
-{
+static void gen_op_pre_decr (ast220_t* ast) {
+	symtab_entry_t* variable_entry = symtab_lookup(ast->left->name);				// ^
+	printf("\tADD R0,R%d,#%d\n\tLDR R1,R0,#0\n\tADD R1,R1,#-1\n\tSTR R1,R0,#0\n\tADD R6,R6,#-1\n\tSTR R1,R6,#0\n", 
+		5 - variable_entry->is_global, variable_entry->offset);
 }
 
 /* 
@@ -420,9 +530,10 @@ gen_op_pre_decr (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_post_incr (ast220_t* ast)
-{
+static void gen_op_post_incr (ast220_t* ast) {
+	symtab_entry_t* variable_entry = symtab_lookup(ast->left->name);				// ^^
+	printf("\tADD R0,R%d,#%d\n\tLDR R1,R0,#0\n\tADD R2,R1,#1\n\tSTR R2,R0,#0\n\tADD R6,R6,#-1\n\tSTR R1,R6,#0\n", 
+		5 - variable_entry->is_global, variable_entry->offset);
 }
 
 /* 
@@ -435,9 +546,10 @@ gen_op_post_incr (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_post_decr (ast220_t* ast)
-{
+static void gen_op_post_decr (ast220_t* ast) {
+	symtab_entry_t* variable_entry = symtab_lookup(ast->left->name);				// ^^^
+	printf("\tADD R0,R%d,#%d\n\tLDR R1,R0,#0\n\tADD R2,R1,#-1\n\tSTR R2,R0,#0\n\tADD R6,R6,#-1\n\tSTR R1,R6,#0\n", 
+		5 - variable_entry->is_global, variable_entry->offset);
 }
 
 /* 
@@ -449,9 +561,11 @@ gen_op_post_decr (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_add (ast220_t* ast)
-{
+static void gen_op_add (ast220_t* ast) {
+	gen_expression(ast->left);									// Calc. arg 1
+	gen_expression(ast->right);									// Calc. arg 2
+	printf("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tLDR R0,R6,#0\n\tADD R6,R6,#1\n");			// R0 - Arg 1, R1 - Arg 2
+	printf("\tADD R0,R0,R1\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n");
 }
 
 /* 
@@ -463,9 +577,11 @@ gen_op_add (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_sub (ast220_t* ast)
-{
+static void gen_op_sub (ast220_t* ast) {
+	gen_expression(ast->left);									// ^
+	gen_expression(ast->right);
+	printf("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tLDR R0,R6,#0\n\tADD R6,R6,#1\n");
+	printf("\tNOT R1,R1\n\tADD R1,R1,#1\n\tADD R0,R0,R1\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n");
 }
 
 /* 
@@ -478,9 +594,14 @@ gen_op_sub (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_mult (ast220_t* ast)
-{
+static void gen_op_mult (ast220_t* ast) {
+	gen_expression(ast->left);									// ^^
+	gen_expression(ast->right);
+	ece220_label_t* subroutine_label = label_create();
+	ece220_label_t* instr_label = label_create();
+	printf("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tLDR R0,R6,#0\n\tADD R6,R6,#1\n");			// R3 - Addr. of target function
+	printf("\tLD R3,%s\n\tJSRR R3\n\tBRnzp %s\n%s\n\t.FILL MULTIPLY\n%s\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n", 
+		label_value(subroutine_label), label_value(instr_label), label_value(subroutine_label), label_value(instr_label));
 }
 
 /* 
@@ -493,9 +614,14 @@ gen_op_mult (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_div (ast220_t* ast)
-{
+static void gen_op_div (ast220_t* ast) {
+	gen_expression(ast->left);									// ^^^
+	gen_expression(ast->right);
+	ece220_label_t* subroutine_label = label_create();
+	ece220_label_t* instr_label = label_create();
+	printf("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tLDR R0,R6,#0\n\tADD R6,R6,#1\n");
+	printf("\tLD R3,%s\n\tJSRR R3\n\tBRnzp %s\n%s\n\t.FILL DIVIDE\n%s\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n", 
+		label_value(subroutine_label), label_value(instr_label), label_value(subroutine_label), label_value(instr_label));
 }
 
 /* 
@@ -509,9 +635,14 @@ gen_op_div (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_mod (ast220_t* ast)
-{
+static void gen_op_mod (ast220_t* ast) {
+	gen_expression(ast->left);									// ^^^^
+	gen_expression(ast->right);
+	ece220_label_t* subroutine_label = label_create();
+	ece220_label_t* instr_label = label_create();
+	printf("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tLDR R0,R6,#0\n\tADD R6,R6,#1\n");
+	printf("\tLD R3,%s\n\tJSRR R3\n\tBRnzp %s\n%s\n\t.FILL MODULUS\n%s\n\tADD R6,R6,#-1\n\tSTR R0,R6,#0\n", 
+		label_value(subroutine_label), label_value(instr_label), label_value(subroutine_label), label_value(instr_label));
 }
 
 /* 
@@ -523,9 +654,9 @@ gen_op_mod (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_negate (ast220_t* ast)
-{
+static void gen_op_negate (ast220_t* ast) {
+	gen_expression(ast->left);									// Calc. arg
+	printf("\tLDR R0,R6,#0\n\tNOT R0,R0\n\tADD R0,R0,#1\n\tSTR R0,R6,#0\n");			// R0 - arg -> -arg
 }
 
 /* 
@@ -538,9 +669,13 @@ gen_op_negate (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_log_not (ast220_t* ast)
-{
+static void gen_op_log_not (ast220_t* ast) {
+	ece220_label_t* branch_label = label_create();
+	gen_expression(ast->left);									// Calc. arg
+	printf("\tAND R2,R2,#0\n\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R0,#0\n\tBRnp %s\n\tADD R2,R2,#1\n", 
+		label_value(branch_label));								// R2 - Result
+	printf("%s\n\tADD R6,R6,#-1\n\tSTR R2,R6,#0\n", 
+		label_value(branch_label));
 }
 
 /* 
@@ -555,9 +690,17 @@ gen_op_log_not (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_log_or (ast220_t* ast)
-{
+static void gen_op_log_or (ast220_t* ast) {
+	ece220_label_t* cmptf_label = label_create();
+	ece220_label_t* cmptt_label = label_create();
+	gen_expression(ast->left);									// Calc. arg 1
+	printf("\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R0,#0\n");
+	gen_long_branch(5, cmptf_label);								// Shortcut when arg 1 is true
+	gen_expression(ast->right);									// Calc. arg 2
+	printf("\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R0,#0\n\tBRnp %s\n\tAND R2,R2,#0\n\tBRnzp %s\n", 
+		label_value(cmptf_label), label_value(cmptt_label));					// R2 - Result
+	printf("%s\n\tAND R2,R2,#0\n\tADD R2,R2,#1\n%s\n\tADD R6,R6,#-1\n\tSTR R2,R6,#0\n", 
+		label_value(cmptf_label), label_value(cmptt_label));
 }
 
 /* 
@@ -572,9 +715,17 @@ gen_op_log_or (ast220_t* ast)
  *   RETURN VALUE: none
  *   SIDE EFFECTS: prints LC-3 instructions
  */
-static void 
-gen_op_log_and (ast220_t* ast)
-{
+static void gen_op_log_and (ast220_t* ast) {
+	ece220_label_t* cmptf_label = label_create();
+	ece220_label_t* cmptt_label = label_create();
+	gen_expression(ast->left);									// ^
+	printf("\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R0,#0\n");
+	gen_long_branch(2, cmptf_label);								// Shortcut when arg 1 is false
+	gen_expression(ast->right);
+	printf("\tLDR R0,R6,#0\n\tADD R6,R6,#1\n\tADD R0,R0,#0\n\tBRz %s\n\tAND R2,R2,#0\n\tADD R2,R2,#1\n\tBRnzp %s\n", 
+		label_value(cmptf_label), label_value(cmptt_label));
+	printf("%s\n\tAND R2,R2,#0\n%s\n\tADD R6,R6,#-1\n\tSTR R2,R6,#0\n", 
+		label_value(cmptf_label), label_value(cmptt_label));
 }
 
 /* 
@@ -599,7 +750,7 @@ gen_comparison (ast220_t* ast, const char* false_cond)
     false_label = label_create ();
     gen_expression (ast->left);
     gen_expression (ast->right);
-    printf ("\tLDR R1,R6,#0\n\tLDR R0,R6,#1\n\tADD R6,R6,#2\n");
+    printf ("\tLDR R1,R6,#0\n\tADD R6,R6,#1\n\tLDR R0,R6,#0\n\tADD R6,R6,#1\n");
     printf ("\tAND R2,R2,#0\n\tNOT R1,R1\n\tADD R1,R1,#1\n\tADD R0,R0,R1\n");
     printf ("\tBR%s %s\n\tADD R2,R2,#1\n", false_cond,
 	    label_value (false_label));
